51796b1c6b679762d8bf6797b0cdbbfb12f0affe
NYH0227 <agent960227@gmail.com>
2023-03-22 10:18:44 +0900

    update pathlib 변경

--- MetaHuman-DNA-Calibration-main/dna_viewer/util/import_util.py
+++ MetaHuman-DNA-Calibration-main/dna_viewer/util/import_util.py
@@ -1,7 +1,7 @@
 from importlib.machinery import SourceFileLoader
 from importlib.util import module_from_spec, spec_from_loader
 from pathlib import Path
-
+import os
 
 from ..util.error import DNAViewerError
 
@@ -17,6 +17,26 @@ class Import:
             and path_obj.exists()
             and path_obj.is_file()
             and path_obj.suffix == ".py"
+        ):
+            spec = spec_from_loader(name, SourceFileLoader(name, path))
+            module = module_from_spec(spec)
+            spec.loader.exec_module(module)
+            return module
+        raise DNAViewerError("File"+ path+" is not found!")
+    
+
+
+class Import:
+    
+    @staticmethod
+    def source_py_file(name, path):
+
+        path_obj = os.path.abspath(path)
+        if (
+            path
+            and os.path.exists(path)
+            and os.path.isfile(path)
+            and os.path.splitext(path)[1] == ".py"
         ):
             spec = spec_from_loader(name, SourceFileLoader(name, path))
             module = module_from_spec(spec)
