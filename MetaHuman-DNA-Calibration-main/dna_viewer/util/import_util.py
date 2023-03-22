f6ad244ff1a2f9420757937ecfa7172dd9ecfd98
NYH0227 <agent960227@gmail.com>
2023-03-22 11:22:23 +0900

    Update import_util.py

--- MetaHuman-DNA-Calibration-main/dna_viewer/util/import_util.py
+++ MetaHuman-DNA-Calibration-main/dna_viewer/util/import_util.py
@@ -1,33 +1,9 @@
-from importlib.machinery import SourceFileLoader
-from importlib.util import module_from_spec, spec_from_loader
-from pathlib import Path
+import imp
 import os
 
 from ..util.error import DNAViewerError
 
-
 class Import:
-
-    @staticmethod
-    def source_py_file(name, path):
-
-        path_obj = Path(path.strip())
-        if (
-            path
-            and path_obj.exists()
-            and path_obj.is_file()
-            and path_obj.suffix == ".py"
-        ):
-            spec = spec_from_loader(name, SourceFileLoader(name, path))
-            module = module_from_spec(spec)
-            spec.loader.exec_module(module)
-            return module
-        raise DNAViewerError("File"+ path+" is not found!")
-    
-
-
-class Import:
-    
     @staticmethod
     def source_py_file(name, path):
 
@@ -38,8 +14,14 @@ class Import:
             and os.path.isfile(path)
             and os.path.splitext(path)[1] == ".py"
         ):
-            spec = spec_from_loader(name, SourceFileLoader(name, path))
-            module = module_from_spec(spec)
-            spec.loader.exec_module(module)
+            # spec = spec_from_loader(name, SourceFileLoader(name, path))
+            # module = module_from_spec(spec)
+            # spec.loader.exec_module(module)
+
+            module = imp.load_source(name, path)
+
             return module
         raise DNAViewerError("File"+ path+" is not found!")
+    
+
+
