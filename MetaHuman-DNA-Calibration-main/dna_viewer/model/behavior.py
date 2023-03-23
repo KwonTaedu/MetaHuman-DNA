c7a8e51a526625491f783ee0bec72e070cdd3b9a
NYH0227 <agent960227@gmail.com>
2023-03-23 18:17:31 +0900

    update init 변경

--- MetaHuman-DNA-Calibration-main/dna_viewer/model/behavior.py
+++ MetaHuman-DNA-Calibration-main/dna_viewer/model/behavior.py
@@ -1,50 +1,53 @@
+
+
 class ConditionalTable:
-    def __init__(self,from_values=[],to_values=[],slope_values=[],cut_values= [],inputs=[],outputs=[]):
-        self.from_values = from_values
-        self.to_values = to_values
-        self.slope_values = slope_values
-        self.cut_values = cut_values
-        self.inputs = inputs
-        self.outputs = outputs
+    def __init__(self,from_values=None,to_values=None,slope_values=None,cut_values= None,inputs=None,outputs=None):
+        self.from_values = from_values if from_values else []
+        self.to_values = to_values if to_values else []
+        self.slope_values = slope_values if slope_values else []
+        self.cut_values = cut_values if cut_values else []
+        self.inputs = inputs if inputs else []
+        self.outputs = outputs if outputs else []
+    
 
 class PSDMatrix:
-    def __init__(self,count=None,rows=[],columns=[],values=[]):
-        self.count = count
-        self.rows = rows
-        self.columns = columns
-        self.values = values
+    def __init__(self,count=None,rows=None,columns=None,values=None):
+        self.count = count 
+        self.rows = rows if rows else []
+        self.columns = columns if columns else []
+        self.values = values if values else []
 
 class JointGroup:
-    def __init__(self,lods=[],values=[],joints=[],inputs=[],outputs=[]):
-        self.lods = lods
-        self.values = values
-        self.joints = joints
-        self.inputs = inputs
-        self.outputs = outputs
+    def __init__(self,lods=None,values=None,joints=None,inputs=None,outputs=None):
+        self.lods = lods if lods else []
+        self.values = values if values else []
+        self.joints = joints if joints else []
+        self.inputs = inputs if inputs else []
+        self.outputs = outputs if outputs else []
 
 class BlendShapesData:
-    def __init__(self,lods=[],inputs=[],outputs=[]):
-        self.lods = lods
-        self.inputs = inputs
-        self.outputs = outputs
+    def __init__(self,lods=None,inputs=None,outputs=None):
+        self.lods = lods if lods else []
+        self.inputs = inputs if inputs else []
+        self.outputs = outputs if outputs else []
 
 
 class AnimatedMapsData:
-    def __init__(self,lods=[],conditional_table = ConditionalTable()):
-        self.lods = lods
-        self.conditional_table = conditional_table
+    def __init__(self,lods=None,conditional_table = None):
+        self.lods = lods if lods else []
+        self.conditional_table = conditional_table if conditional_table else ConditionalTable()
 
 class JointsData:
-    def __init__(self,joint_row_count=None,joint_column_count=None,joint_variable_attribute_indices=[],joint_groups=[]):
+    def __init__(self,joint_row_count=None,joint_column_count=None,joint_variable_attribute_indices=None,joint_groups=None):
         self.joint_row_count = joint_row_count
         self.joint_column_count = joint_column_count
-        self.joint_variable_attribute_indices = joint_variable_attribute_indices
-        self.joint_groups = joint_groups
+        self.joint_variable_attribute_indices = joint_variable_attribute_indices if joint_variable_attribute_indices else []
+        self.joint_groups = joint_groups if joint_groups else []
 
 class Behavior:
-    def __init__(self,gui_to_raw=ConditionalTable(),psd=PSDMatrix(),blend_shapes=BlendShapesData(),animated_maps=AnimatedMapsData(),joints=JointsData()):
-        self.gui_to_raw = gui_to_raw
-        self.psd = psd
-        self.blend_shapes = blend_shapes
-        self.animated_maps = animated_maps
-        self.joints = joints
+    def __init__(self,gui_to_raw=None,psd=None,blend_shapes=None,animated_maps=None,joints=None):
+        self.gui_to_raw = gui_to_raw if gui_to_raw else ConditionalTable()
+        self.psd = psd if psd else PSDMatrix()
+        self.blend_shapes = blend_shapes if blend_shapes else BlendShapesData()
+        self.animated_maps = animated_maps if animated_maps else AnimatedMapsData()
+        self.joints = joints if joints else JointsData()
