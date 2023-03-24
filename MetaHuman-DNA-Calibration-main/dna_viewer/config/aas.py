89a85a6f480cf1fbc220f07e2795262544b18256
NYH0227 <agent960227@gmail.com>
2023-03-24 10:21:59 +0900

    Update aas.py
    필드명 변경

--- MetaHuman-DNA-Calibration-main/dna_viewer/config/aas.py
+++ MetaHuman-DNA-Calibration-main/dna_viewer/config/aas.py
@@ -1,7 +1,7 @@
 class AdditionalAssemblyScript:
     def __init__(self,path = None,module_name = None, method= None,parmeter=None):
         self.path = path 
-        self.module = module_name
+        self.module_name = module_name
         self.method = method if method else "run_after_assemble"
         self.parmeter = parmeter if parmeter else {}
 
